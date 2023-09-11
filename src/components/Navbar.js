a88a13f6396b8f8a91a59dea6762c74d8d001edc
Nikita More <morenikita9670@.com>
2023-09-11 17:45:32 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className="navbar">
+        <div className="container-fluid">
+            <span className="navbar-brand"><i class="fa-regular fa-pen-to-square"></i> Awesome Goals to Achieve</span>
+        </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
